7f73c94fb87f51d6d9388fb631d2d97fed998b90
SephyIIDX <swensson.daniel@gmail.com>
2022-06-05 20:41:27 +0200

    Add solution to fenwick

---
+++ Java/fenwick/src/Main.java
@@ -0,0 +1,38 @@
+
+/**
+ * This program solves the KATTIS problem "fenwick".
+ */
+public class Main {
+	
+	/**
+	 * Reads the input and prints the solution as specified by KATTIS.
+	 */
+	public void runOnKattis() {
+		Kattio io = new Kattio(System.in, System.out);
+		int n = io.getInt();
+		int q = io.getInt();
+
+		FenwickTree ft = new FenwickTree(n);
+		
+		char query;
+		int j;
+		int delta;
+		for (int i = 0; i < q; i++) {
+			query = io.getWord().charAt(0);
+			j = io.getInt();
+			
+			if (query == '+') {
+				delta = io.getInt();
+				ft.add(j, delta);
+			}
+			else { //query == '?'
+				io.println(ft.sum(j));
+			}
+		}
+		io.close();
+	}
+	
+	public static void main(String[] args) {
+		new Main().runOnKattis();
+	}
+}
