2ee77c996a95125d322c538f1a46342714f48fa5
SephyIIDX <swensson.daniel@gmail.com>
2022-06-07 20:27:31 +0200

    Flush and close IO

--- Kotlin/peasoup/src/main/kotlin/Main.kt
+++ Kotlin/peasoup/src/main/kotlin/Main.kt
@@ -4,27 +4,29 @@
  */
 fun main() {
     val io = Kattio()
+
     fun solve(): String? {
         val n = io.readInt
+
         for (i in 0 until n) {
             val k = io.readInt
             val restaurant = io.readLine
             var hasPeaSoup = false
             var hasPancakes = false
+
             for (j in 0 until k) {
                 val line = io.readLine
-                if (!hasPeaSoup && line.equals("pea soup")) {
+                if (!hasPeaSoup && line.equals("pea soup"))
                     hasPeaSoup = true
-                } else if (!hasPancakes && line.equals("pancakes")) {
+                else if (!hasPancakes && line.equals("pancakes"))
                     hasPancakes = true
-                }
-                if (hasPeaSoup && hasPancakes) {
+                
+                if (hasPeaSoup && hasPancakes)
                     return restaurant
-                }
             }
         }
         return "Anywhere is fine I guess"
     }
-
     io.println(solve())
+    io.close()
 }
