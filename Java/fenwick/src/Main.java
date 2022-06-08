b689d52d590026eb6da01c9ef150cfb3838ea696
SephyIIDX <swensson.daniel@gmail.com>
2022-06-08 19:48:40 +0200

    Fix indentation

--- Java/fenwick/src/Main.java
+++ Java/fenwick/src/Main.java
@@ -3,36 +3,35 @@
  * Solves the Kattis problem: fenwick
  */
 public class Main {
-	
-	/**
-	 * Reads the input and prints the solution as specified by Kattis.
-	 */
-	public void runOnKattis() {
-		Kattio io = new Kattio(System.in, System.out);
-		int n = io.getInt();
-		int q = io.getInt();
 
-		FenwickTree ft = new FenwickTree(n);
-		
-		char query;
-		int j;
-		int delta;
-		for (int i = 0; i < q; i++) {
-			query = io.getWord().charAt(0);
-			j = io.getInt();
-			
-			if (query == '+') {
-				delta = io.getInt();
-				ft.add(j, delta);
-			}
-			else { //query == '?'
-				io.println(ft.sum(j));
-			}
-		}
-		io.close();
-	}
-	
-	public static void main(String[] args) {
-		new Main().runOnKattis();
-	}
+    /**
+     * Reads the input and prints the solution as specified by Kattis.
+     */
+    public void runOnKattis() {
+        Kattio io = new Kattio(System.in, System.out);
+        int n = io.getInt();
+        int q = io.getInt();
+
+        FenwickTree ft = new FenwickTree(n);
+
+        char query;
+        int j;
+        int delta;
+        for (int i = 0; i < q; i++) {
+            query = io.getWord().charAt(0);
+            j = io.getInt();
+
+            if (query == '+') {
+                delta = io.getInt();
+                ft.add(j, delta);
+            } else { // query == '?'
+                io.println(ft.sum(j));
+            }
+        }
+        io.close();
+    }
+
+    public static void main(String[] args) {
+        new Main().runOnKattis();
+    }
 }
