0b37177ccf7caf1bc4e0e2400b0333a8f3c8f376
SephyIIDX <swensson.daniel@gmail.com>
2022-06-08 19:12:52 +0200

    Fix indentation

--- Java/borg/src/Pair.java
+++ Java/borg/src/Pair.java
@@ -3,44 +3,42 @@ public class Pair<A, B> {
     final private B second;
 
     public Pair(A first, B second) {
-    	super();
-    	this.first = first;
-    	this.second = second;
+        super();
+        this.first = first;
+        this.second = second;
     }
 
     public int hashCode() {
-    	int hashFirst = first != null ? first.hashCode() : 0;
-    	int hashSecond = second != null ? second.hashCode() : 0;
+        int hashFirst = first != null ? first.hashCode() : 0;
+        int hashSecond = second != null ? second.hashCode() : 0;
 
-    	return (hashFirst + hashSecond) * hashSecond + hashFirst;
+        return (hashFirst + hashSecond) * hashSecond + hashFirst;
     }
 
     @SuppressWarnings("rawtypes")
-	public boolean equals(Object other) {
-    	if (other instanceof Pair) {
-    		Pair otherPair = (Pair) other;
-    		return 
-    		((  this.first == otherPair.first ||
-    			( this.first != null && otherPair.first != null &&
-    			  this.first.equals(otherPair.first))) &&
-    		 (	this.second == otherPair.second ||
-    			( this.second != null && otherPair.second != null &&
-    			  this.second.equals(otherPair.second))) );
-    	}
+    public boolean equals(Object other) {
+        if (other instanceof Pair) {
+            Pair otherPair = (Pair) other;
+            return ((this.first == otherPair.first ||
+                    (this.first != null && otherPair.first != null &&
+                    this.first.equals(otherPair.first))) &&
+                    (this.second == otherPair.second ||
+                    (this.second != null && otherPair.second != null &&
+                    this.second.equals(otherPair.second))));
+        }
 
-    	return false;
+        return false;
     }
 
-    public String toString()
-    { 
-           return "(" + first + ", " + second + ")"; 
+    public String toString() {
+        return "(" + first + ", " + second + ")";
     }
 
     public A getFirst() {
-    	return first;
+        return first;
     }
 
     public B getSecond() {
-    	return second;
+        return second;
     }
 }
