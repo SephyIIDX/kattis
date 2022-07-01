eb0b36779da4769c19c5581ccfea848fd22c0a37
SephyIIDX <swensson.daniel@gmail.com>
2022-07-01 02:04:57 +0200

    Add solution to bestcompression

---
+++ C/bestcompression/src/main.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+/*
+ * Solves the Kattis problem: bestcompression
+ */
+int main()
+{
+	long long N;
+	int b;
+
+	scanf("%lld %d", &N, &b);
+
+	if (N < (2L << b))
+		printf("yes\n");
+	else
+		printf("no\n");
+
+	return 0;
+}
